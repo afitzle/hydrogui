fb8bc679302b8b138807d8dfdc2b4592fa736bff
root <root@localhost.localdomain>
2020-05-07 00:38:21 -0500

    adding correct dot file for testing parser

--- Hydrogen_GUI/Hydrogen_GUI/run.sh
+++ Hydrogen_GUI/Hydrogen_GUI/run.sh
@@ -1,2 +1,2 @@
-#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
 g++ App.cpp MainWindow.cpp `wx-config --cxxflags --libs` -o hydrogui && ./hydrogui
